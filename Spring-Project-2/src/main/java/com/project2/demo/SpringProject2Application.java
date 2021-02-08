de527bf69fd35d7dedbb8dcbcb3dd462c9150881
tryingthisserverout <phungbros@gmail.com>
2021-02-08 14:02:44 -0800

    Created Starter Spring Project.

---
+++ Spring-Project-2/src/main/java/com/project2/demo/SpringProject2Application.java
@@ -0,0 +1,13 @@
+package com.project2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject2Application.class, args);
+	}
+
+}
---
+++ Spring-Project-2/src/test/java/com/project2/demo/SpringProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
