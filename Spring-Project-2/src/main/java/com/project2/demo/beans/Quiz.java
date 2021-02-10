7eb9bfe7e92a2d42f9b0d5bfe6ecaf4d29f1f4c7
batur <batur@DESKTOP-M1BDIK3>
2021-02-10 11:13:16 -0500

    user-quiz changes

---
+++ Spring-Project-2/src/main/java/com/project2/demo/beans/Quiz.java
@@ -0,0 +1,73 @@
+package com.project2.beans;
+
+import com.project2.beans.User;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="quizzes")
+public class Quiz {
+
+	@Id
+	@Column(name="ID",updatable=false)
+	private int id;
+	
+	@Column(name="USERID")
+	private User userid;
+	
+	@Column(name="NAME")
+	private String name;
+
+	public Quiz() {
+		super();
+	}
+
+	public Quiz(int id, User userid, String name) {
+		super();
+		this.id = id;
+		this.userid = userid;
+		this.name = name;
+	}
+
+	public Quiz(User userid, String name) {
+		super();
+		this.userid = userid;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUserid() {
+		return userid;
+	}
+
+	public void setUserid(User userid) {
+		this.userid = userid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Quiz [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+}
