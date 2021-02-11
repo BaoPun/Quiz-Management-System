12906fe869a94716f3401e2248b6bc951aec0add
tryingthisserverout <phungbros@gmail.com>
2021-02-11 07:24:44 -0800

    fixed controller

--- Spring-Project-2/src/main/java/com/project2/demo/controllers/SecureController.java
+++ Spring-Project-2/src/main/java/com/project2/demo/controllers/SecureController.java
@@ -13,10 +13,23 @@ public class SecureController {
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	@GetMapping("grade")
-	public String grade(Model model) {
-		return "grade";
+	@GetMapping("/grade")
+	public String grade_page(Model model) {
+		return "s/grade";
 	}
-	
+
+	@GetMapping("/question")
+	public String question_page(Model model) {
+		return "s/question";
+	}
+
+	@GetMapping("/quiz_selector")
+	public String quiz_selector_page(Model model) {
+		return "s/quiz_selector";
+	}
+
+	@GetMapping("/teacher")
+	public String teacher_page(Model model) {
+		return "s/teacher";
+	}
 }
