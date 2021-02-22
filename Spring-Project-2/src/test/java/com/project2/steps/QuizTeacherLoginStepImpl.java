74a8be85c85d8332de8aa4c0aec1c955408e5bce
baturhankontas <33671381+baturhankontas@users.noreply.github.com>
2021-02-22 05:35:47 -0500

    Update QuizTeacherLoginStepImpl.java

--- Spring-Project-2/src/test/java/com/project2/steps/QuizTeacherLoginStepImpl.java
+++ Spring-Project-2/src/test/java/com/project2/steps/QuizTeacherLoginStepImpl.java
@@ -18,31 +18,36 @@ public class QuizTeacherLoginStepImpl {
 	@Given("^Teacher is on the Quiz Home Page$")
 	public void teacher_is_on_the_Quiz_Home_Page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+		driver.get("http://localhost:8080/");
+		Thread.sleep(1000);
 	}
 
 	@When("^The teacher fill username field with \"([^\"]*)\" for login$")
-	public void the_teacher_fill_username_field_with_for_login(String arg1) throws Throwable {
+	public void the_teacher_fill_username_field_with_for_login(String username) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+		quizlogin.username.sendKeys(username);
+		Thread.sleep(1000);
 	}
 
 	@When("^The teacher fill password with \"([^\"]*)\" for login$")
-	public void the_teacher_fill_password_with_for_login(String arg1) throws Throwable {
+	public void the_teacher_fill_password_with_for_login(String password) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+		quizlogin.password.sendKeys(password);
+		Thread.sleep(1000);
 	}
 
 	@When("^The teacher press the \"([^\"]*)\" button for login$")
 	public void the_teacher_press_the_button_for_login(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+		quizlogin.loginButton.click();
+		Thread.sleep(1000);
 	}
 
 	@Then("^The teacher should be on the Teacher Page after login$")
 	public void the_teacher_should_be_on_the_Teacher_Page_after_login() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+		Assert.assertEquals("Teacher Administration Page",driver.getTitle());
+		Thread.sleep(1000);
 	}
 
 	
