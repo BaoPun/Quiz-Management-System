f0841c4d18ef5b3fb72c3fe05a73f2f5ff68e76b
baturhankontas <33671381+baturhankontas@users.noreply.github.com>
2021-02-22 13:05:23 -0500

    Update QuizMake.java

--- Spring-Project-2/src/test/java/com/project2/pages/QuizMake.java
+++ Spring-Project-2/src/test/java/com/project2/pages/QuizMake.java
@@ -9,29 +9,51 @@ public class QuizMake {
 
 	public WebDriver driver;
 	
-	@FindBy(id = "quiz_name")
-	public WebElement title;
 	
-	@FindBy(id = "question")
-	public WebElement question;
+	@FindBy(id = "quiz-name-input")
+	public WebElement quizNameInput;
 	
-	@FindBy(id = "answer1")
+	@FindBy(id = "question-box-0")
+	public WebElement question1;
+	
+	@FindBy(id = "text-box-0-0")
 	public WebElement answer1;
 
-	@FindBy(id = "answer2")
+	@FindBy(id = "text-box-0-1")
 	public WebElement answer2;
 
-	@FindBy(id = "answer3")
+	@FindBy(id = "text-box-0-2")
 	public WebElement answer3;
 
-	@FindBy(id = "answer4")
+	@FindBy(id = "text-box-0-3")
 	public WebElement answer4;
+ 
+	@FindBy(id="checkbox-0-0")
+	public WebElement correct1; 
 	
-	@FindBy(id = "add_button")
+	@FindBy(id = "question-box-1")
+	public WebElement question2;
+	
+	@FindBy(id = "text-box-1-0")
+	public WebElement answer21;
+
+	@FindBy(id = "text-box-2-1")
+	public WebElement answer22;
+
+	@FindBy(id = "text-box-1-2")
+	public WebElement answer23;
+
+	@FindBy(id = "text-box-1-3")
+	public WebElement answer24;
+ 
+	@FindBy(id="checkbox-1-0")
+	public WebElement correct2; 
+	
+	@FindBy(id = "addQuestion")
 	public WebElement add_button;
 	
-	@FindBy(id = "complete_button")
-	public WebElement complete_button;
+	@FindBy(id = "submit_quiz")
+	public WebElement submit_quiz;
 
 	public QuizMake(WebDriver driver) {
 		this.driver = driver;
