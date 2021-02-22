90732477c0a7daa47d143fc67f6e231bed1460c9
emilytroll <76971112+emilytroll@users.noreply.github.com>
2021-02-21 23:03:39 -0500

    ssssssssss

--- Spring-Project-2/src/main/resources/static/s/js/question.js
+++ Spring-Project-2/src/main/resources/static/s/js/question.js
@@ -1,5 +1,4 @@
 $(function () {
-<<<<<<< HEAD
 	$('.answer').on('click',answerFunc);
 	$('.previous').on('click',something);
 	$('.next').on('click',something);
@@ -10,7 +9,4 @@ function answerFunc()
 	$('.highlight').removeClass('highlight');
     $(this).addClass('highlight')
 }
-=======
-	$('.student-list-item').on('click',studentListItemClick);
-})
->>>>>>> refs/remotes/origin/et_question
+
