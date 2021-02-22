9f266fbebd5fd28ff9f5b0e467735eb19a038663
baturhankontas <33671381+baturhankontas@users.noreply.github.com>
2021-02-22 05:38:26 -0500

    Update QuizStudentRunner.java

--- Spring-Project-2/src/test/java/com/project2/runners/QuizStudentRunner.java
+++ Spring-Project-2/src/test/java/com/project2/runners/QuizStudentRunner.java
@@ -28,6 +28,13 @@ public class QuizStudentRunner {
 		driver = new ChromeDriver();
 		quizlogin =new QuizLogin(driver);	
 		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	@AfterClass
 	public static void tearDown() {
