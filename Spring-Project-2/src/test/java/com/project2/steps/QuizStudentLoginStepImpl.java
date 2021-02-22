b72f5be45deb126240d10bdfaf96450e6431d56c
baturhankontas <33671381+baturhankontas@users.noreply.github.com>
2021-02-22 05:36:14 -0500

    Update QuizStudentLoginStepImpl.java

--- Spring-Project-2/src/test/java/com/project2/steps/QuizStudentLoginStepImpl.java
+++ Spring-Project-2/src/test/java/com/project2/steps/QuizStudentLoginStepImpl.java
@@ -18,30 +18,35 @@ public class QuizStudentLoginStepImpl {
 	public void the_student_is_on_the_Quiz_Home_Page_for_login() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		driver.get("http://localhost:8080/");
+		Thread.sleep(1000);
 	}
 
 	@When("^The student fill username field with \"([^\"]*)\" for login$")
 	public void the_student_fill_username_field_with_for_login(String username) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		quizLogin.username.sendKeys(username);
+		Thread.sleep(1000);
 	}
 
 	@When("^The student fill password with \"([^\"]*)\" for login$")
 	public void the_student_fill_password_with_for_login(String password) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		quizLogin.password.sendKeys(password);
+		Thread.sleep(1000);
 	}
 
 	@When("^The student press the \"([^\"]*)\" button for login$")
 	public void the_student_press_the_button_for_login(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		quizLogin.loginButton.click();
+		Thread.sleep(1000);
 	}
 
 	@Then("^The student should be on the Student Page for login$")
 	public void the_student_should_be_on_the_Student_Page_for_login() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		Assert.assertEquals("Welcome to the Student Page",driver.getTitle());
+		Assert.assertEquals("Student Page",driver.getTitle());
+		Thread.sleep(1000);
 	}
 	
 	
