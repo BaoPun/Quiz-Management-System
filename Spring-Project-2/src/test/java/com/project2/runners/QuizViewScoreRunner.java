8b6fc3249147932b11472e88d6f0120be956e22e
baturhankontas <33671381+baturhankontas@users.noreply.github.com>
2021-02-22 05:39:21 -0500

    Update QuizViewScoreRunner.java

--- Spring-Project-2/src/test/java/com/project2/runners/QuizViewScoreRunner.java
+++ Spring-Project-2/src/test/java/com/project2/runners/QuizViewScoreRunner.java
@@ -26,6 +26,12 @@ public class QuizViewScoreRunner {
 
 		driver = new ChromeDriver();
 		quizlogin =new QuizLogin(driver);	
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	@AfterClass
